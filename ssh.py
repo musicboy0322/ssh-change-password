7ea609dad294ee8c1208ffae2976e80f04450220
Kyle <plusultra0322@gmail.com>
2023-07-18 10:06:35 +0800

    ADD-pop result window(as stated above ,now can pop result window)

--- ssh.py
+++ ssh.py
@@ -4,6 +4,7 @@ import paramiko
 import win32com.client as win32
 from datetime import datetime, timedelta
 import random
+import tkinter as tk
 
 # import config file
 config = configparser.ConfigParser()
@@ -15,7 +16,7 @@ password = config['TARGET']['PASSWORD']
 hostname = config['TARGET']['HOSTNAME']
 port = int(config['TARGET']['PORT'])
 
-# get config information(new password) and choose random password
+# get config information(new password) and choose random password without repeating the same password
 newPasswordSplit = config['NEW']['PASSWORD'].split(',')
 newPassword = newPasswordSplit[random.randint(0,len(newPasswordSplit)-1)]
 while newPassword == password :
@@ -32,15 +33,15 @@ email = config['SEND']['EMAIL']
 outlook = win32.Dispatch('outlook.application')
 mapi = outlook.GetNamespace("MAPI")
 
-chaneYesNo = False;
+changeYesNo = False
 for num in range(len(mapi.Folders)) :
-    received_dt = datetime.now() - timedelta(days = 20)
+    received_dt = datetime.now() - timedelta(days = 1)
     received_dt = received_dt.strftime('%m/%d/%Y %H:%M')
     messages = mapi.Folders(num + 1).Folders('收件匣').Items
     messages = messages.Restrict("[ReceivedTime] >='" + received_dt + "'")
     for msg in list(messages):
         if 'WARNING' in str(msg) and int(str(msg).split(' ')[8]) <= 0:
-            changeYesNo = True;
+            changeYesNo = True
 
 if changeYesNo :
     try :
@@ -79,9 +80,34 @@ if changeYesNo :
 
         logging.info(f'Your new password : {newPassword}')
 
+        # synchronize changing config file's password information
+        config.set('TARGET', 'PASSWORD', newPassword)
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
+
+        # pop result window
+        window = tk.Tk()
+        window.title("Result")
+        window.geometry('250x90')
+        l = tk.Label(window,text="Password has changed", font=("Arial", 12), width=20,height=10)
+        l.pack()
+        window.mainloop()
+
     except Exception as e:
         print(e)
+else:
+    # pop result window
+    window = tk.Tk()
+    window.title("Result")
+    window.geometry('250x90')
+    l = tk.Label(window,text="Not yet to change", font=("Arial", 12), width=20,height=10)
+    l.pack()
+    window.mainloop()
 
+
+
+
+# customize function
 def sendingEmail():
     # sending email about the result of changing password
     mail = outlook.CreateItem(0)
